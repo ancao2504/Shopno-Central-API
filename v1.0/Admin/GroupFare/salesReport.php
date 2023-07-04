896e7adfb57cc35e894d6ebd664d3eb692bec225
tonmoychiran <tgchiran23@gmail.com>
2023-07-04 08:47:18 +0600

    group fare markup added

---
+++ v1.0/Admin/GroupFare/salesReport.php
@@ -0,0 +1,44 @@
+<?php
+
+
+require '../../config.php';
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if($_SERVER["REQUEST_METHOD"] == "GET")
+{   
+
+    $sql="SELECT bookingId, gds, vendor, platform, remarks, gds_segment, netcost, iMarkup, dMarkup, invoice  
+    FROM booking 
+    ORDER BY bookingId DESC";
+    $response=$conn->query($sql)->fetch_all(MYSQLI_ASSOC);
+
+    if(!empty($response))
+    {
+        echo json_encode($response);
+    }
+    else
+    {
+        $response["status"] = "Failed";
+        $response["message"] = "Data Not Found";
+        
+        echo json_encode($response);
+    }
+    
+    
+    
+}
+else
+{
+    $response["status"] = "Failed";
+    $response["message"] = "Wrong Request Method";
+    
+    echo json_encode($response);
+}
+    $conn->close();
+
+
+?>
