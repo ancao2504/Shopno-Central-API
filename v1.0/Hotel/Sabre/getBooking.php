8ec8de4d54a9d70dcd84f2055fc74cce0497798f
sohan724flyfar <sohan@flyfar.tech>
2023-12-03 09:59:02 +0600

    Update getBooking

--- v1.0/Hotel/Sabre/getBooking.php
+++ v1.0/Hotel/Sabre/getBooking.php
@@ -48,6 +48,47 @@ if (array_key_exists("all", $_GET)) {
         $response['message'] = "Booking Data Not Found";
         echo json_encode($response);
     }
+}elseif (array_key_exists("all", $_GET)&& array_key_exists("agentId", $_GET)) {
+    $agentId = $_GET['grantId'];
+    $query = "SELECT * FROM `hotel_booking` WHERE agentId = $agentId ORDER BY id DESC";
+    $result = $conn->query($query);
+
+    if ($result && $result->num_rows > 0) {
+        $return_arr = array();
+        while ($row = $result->fetch_assoc()) {
+            $agentId = $row['agentId'];
+
+            $agentQuery = $conn->query("SELECT * FROM agent WHERE agentId='$agentId'");
+            $agentData = $agentQuery->fetch_assoc();
+
+            if ($agentData) {
+                $companyName = $agentData['company'];
+                $companyPhone = $agentData['phone'];
+                $bookedBy = $agentData['name'];
+            }
+
+            $passengerQuery = $conn->query("SELECT * FROM hotel_passengers WHERE agentId='$agentId'");
+            $passengerInfo = $passengerQuery->fetch_assoc();
+
+            $paymentInfoQuery = $conn->query("SELECT * FROM hotel_payment_info WHERE agentId='$agentId'");
+            $paymentInfo = $paymentInfoQuery->fetch_assoc();
+
+            $response = $row;
+            $response['companyName'] = $companyName ?? "";
+            $response['companyPhone'] = $companyPhone ?? "";
+            $response['bookedBy'] = $bookedBy ?? "";
+            $response['passengerInfo'] = [$passengerInfo] ?? [];
+            $response['paymentInfo'] = $paymentInfo ?? [];
+
+            $return_arr[] = $response;
+        }
+
+        echo json_encode($return_arr);
+    } else {
+        $response['status'] = 'error';
+        $response['message'] = "Booking Data Not Found";
+        echo json_encode($response);
+    }
 } elseif (array_key_exists("bookingId", $_GET)) {
     $bookingId = $_GET["bookingId"];
     $sql = "SELECT * FROM `hotel_booking` where bookingId='$bookingId'";
