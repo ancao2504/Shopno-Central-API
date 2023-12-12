3d0f25d7fc5ebd4235f87d28e2b9b464cf62087f
sohan724flyfar <sohan@flyfar.tech>
2023-12-12 17:59:25 +0600

    Update

--- v1.0/Hotel/Sabre/getBooking.php
+++ v1.0/Hotel/Sabre/getBooking.php
@@ -8,7 +8,93 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 $response = array();
 
-if (array_key_exists("all", $_GET)) {
+if (array_key_exists("agentId", $_GET) && array_key_exists("bookingId", $_GET) ) {
+    $agentId = $_GET['agentId'];
+    $bookingId = $_GET['bookingId'];
+    $query = "SELECT * FROM `hotel_booking` WHERE `agentId` = '$agentId' AND `bookingId` = '$bookingId'";
+    $result = $conn->query($query);
+
+    if ($result && $result->num_rows > 0) {
+        $return_arr = array();
+        while ($row = $result->fetch_assoc()) {
+            $agentId = $row['agentId'];
+            $bookingId = $row['bookingId'];
+
+            $agentQuery = $conn->query("SELECT * FROM agent WHERE agentId='$agentId'");
+            $agentData = $agentQuery->fetch_assoc();
+
+            if ($agentData) {
+                $companyName = $agentData['company'];
+                $companyPhone = $agentData['phone'];
+                $bookedBy = $agentData['name'];
+            }
+
+            $passengerQuery = $conn->query("SELECT * FROM hotel_passengers WHERE `bookingId` = '$bookingId' AND agentId='$agentId'");
+            $passengerInfo = $passengerQuery->fetch_assoc();
+
+            $paymentInfoQuery = $conn->query("SELECT * FROM hotel_payment_info WHERE `bookingId` = '$bookingId' AND agentId='$agentId'");
+            $paymentInfo = $paymentInfoQuery->fetch_assoc();
+
+            $response = $row;
+            $response['companyName'] = $companyName ?? "";
+            $response['companyPhone'] = $companyPhone ?? "";
+            $response['bookedBy'] = $bookedBy ?? "";
+            $response['passengerInfo'] = [$passengerInfo] ?? [];
+            $response['paymentInfo'] = $paymentInfo ?? [];
+
+            $return_arr[] = $response;
+        }
+
+        echo json_encode($return_arr);
+    } else {
+        $response['status'] = 'error';
+        $response['message'] = "Booking Data Not Found";
+        echo json_encode($response);
+    }
+}elseif (array_key_exists("userId", $_GET) && array_key_exists("bookingId", $_GET) ) {
+    $userId = $_GET['userId'];
+    $bookingId = $_GET['bookingId'];
+    $query = "SELECT * FROM `hotel_booking` WHERE `userId` = '$userId' AND `bookingId` = '$bookingId'";
+    $result = $conn->query($query);
+
+    if ($result && $result->num_rows > 0) {
+        $return_arr = array();
+        while ($row = $result->fetch_assoc()) {
+            $userId = $row['userId'];
+            $bookingId = $row['bookingId'];
+
+            $userQuery = $conn->query("SELECT * FROM `agent` WHERE `userId`='$userId'");
+            $userData = $userQuery->fetch_assoc();
+
+            if ($userData) {
+                $companyName = $userData['company'];
+                $companyPhone = $userData['phone'];
+                $bookedBy = $userData['name'];
+            }
+
+            $passengerQuery = $conn->query("SELECT * FROM hotel_passengers WHERE `bookingId` = '$bookingId' AND `userId`='$userId'");
+            $passengerInfo = $passengerQuery->fetch_assoc();
+
+            $paymentInfoQuery = $conn->query("SELECT * FROM hotel_payment_info WHERE `bookingId` = '$bookingId' AND `userId`='$userId'");
+            $paymentInfo = $paymentInfoQuery->fetch_assoc();
+
+            $response = $row;
+            $response['companyName'] = $companyName ?? "";
+            $response['companyPhone'] = $companyPhone ?? "";
+            $response['bookedBy'] = $bookedBy ?? "";
+            $response['passengerInfo'] = [$passengerInfo] ?? [];
+            $response['paymentInfo'] = $paymentInfo ?? [];
+
+            $return_arr[] = $response;
+        }
+
+        echo json_encode($return_arr);
+    } else {
+        $response['status'] = 'error';
+        $response['message'] = "Booking Data Not Found";
+        echo json_encode($response);
+    }
+}elseif (array_key_exists("all", $_GET)) {
     $query = "SELECT * FROM `hotel_booking` ORDER BY id DESC";
     $result = $conn->query($query);
 
@@ -166,88 +252,6 @@ if (array_key_exists("all", $_GET)) {
             $response['bookedBy'] = $bookedBy ?? "";
             $response['passengerInfo'] = [$passengerInfo] ?? [];
             $response['paymentInfo'] = $paymentInfo?? [];
-
-            $return_arr[] = $response;
-        }
-
-        echo json_encode($return_arr);
-    } else {
-        $response['status'] = 'error';
-        $response['message'] = "Booking Data Not Found";
-        echo json_encode($response);
-    }
-}elseif (array_key_exists("agentId", $_GET) && array_key_exists("bookingId", $_GET) ) {
-    $agentId = $_GET['agentId'];
-    $query = "SELECT * FROM `hotel_booking` WHERE agentId = $agentId AND bookingId = $bookingId ORDER BY id DESC";
-    $result = $conn->query($query);
-
-    if ($result && $result->num_rows > 0) {
-        $return_arr = array();
-        while ($row = $result->fetch_assoc()) {
-            $agentId = $row['agentId'];
-
-            $agentQuery = $conn->query("SELECT * FROM agent WHERE agentId='$agentId'");
-            $agentData = $agentQuery->fetch_assoc();
-
-            if ($agentData) {
-                $companyName = $agentData['company'];
-                $companyPhone = $agentData['phone'];
-                $bookedBy = $agentData['name'];
-            }
-
-            $passengerQuery = $conn->query("SELECT * FROM hotel_passengers WHERE `bookingId` = '$bookingId' AND agentId='$agentId'");
-            $passengerInfo = $passengerQuery->fetch_assoc();
-
-            $paymentInfoQuery = $conn->query("SELECT * FROM hotel_payment_info WHERE `bookingId` = '$bookingId' AND agentId='$agentId'");
-            $paymentInfo = $paymentInfoQuery->fetch_assoc();
-
-            $response = $row;
-            $response['companyName'] = $companyName ?? "";
-            $response['companyPhone'] = $companyPhone ?? "";
-            $response['bookedBy'] = $bookedBy ?? "";
-            $response['passengerInfo'] = [$passengerInfo] ?? [];
-            $response['paymentInfo'] = $paymentInfo ?? [];
-
-            $return_arr[] = $response;
-        }
-
-        echo json_encode($return_arr);
-    } else {
-        $response['status'] = 'error';
-        $response['message'] = "Booking Data Not Found";
-        echo json_encode($response);
-    }
-}elseif (array_key_exists("userId", $_GET) && array_key_exists("bookingId", $_GET) ) {
-    $userId = $_GET['userId'];
-    $query = "SELECT * FROM `hotel_booking` WHERE `userId` = '$userId' AND `bookingId` = '$bookingId' ORDER BY id DESC";
-    $result = $conn->query($query);
-
-    if ($result && $result->num_rows > 0) {
-        $return_arr = array();
-        while ($row = $result->fetch_assoc()) {
-            $userId = $row['userId'];
-
-            $userQuery = $conn->query("SELECT * FROM `agent` WHERE `userId`='$userId'");
-            $userData = $userQuery->fetch_assoc();
-
-            if ($agentData) {
-                $companyName = $userData['company'];
-                $companyPhone = $userData['phone'];
-                $bookedBy = $userData['name'];
-            }
-
-            $passengerQuery = $conn->query("SELECT * FROM hotel_passengers WHERE `bookingId` = '$bookingId' AND `userId`='$userId'");
-            $passengerInfo = $passengerQuery->fetch_assoc();
-
-            $paymentInfoQuery = $conn->query("SELECT * FROM hotel_payment_info WHERE `bookingId` = '$bookingId' AND `userId`='$userId'");
-            $paymentInfo = $paymentInfoQuery->fetch_assoc();
-
-            $response = $row;
-            $response['companyName'] = $companyName ?? "";
-            $response['companyPhone'] = $companyPhone ?? "";
-            $response['bookedBy'] = $bookedBy ?? "";
-            $response['passengerInfo'] = [$passengerInfo] ?? [];
-            $response['paymentInfo'] = $paymentInfo ?? [];
 
             $return_arr[] = $response;
         }
